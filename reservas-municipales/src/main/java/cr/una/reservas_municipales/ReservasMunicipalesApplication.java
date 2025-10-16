3b153497041493e125793aabb5b8f9a96eef36dc
jordyestrada <jordyestrada282@gmail.com>
2025-10-16 11:38:00 -0600

    Implementacion de Spring-Boot

---
+++ reservas-municipales/src/main/java/cr/una/reservas_municipales/ReservasMunicipalesApplication.java
@@ -0,0 +1,13 @@
+package cr.una.reservas_municipales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasMunicipalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasMunicipalesApplication.class, args);
+	}
+
+}
---
+++ reservas-municipales/src/test/java/cr/una/reservas_municipales/ReservasMunicipalesApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.una.reservas_municipales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasMunicipalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
